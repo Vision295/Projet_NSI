b56f462c679263753ba1dbf24afafce39127f805
Vision295 <78678991+Vision295@users.noreply.github.com>
2022-01-29 22:22:29 +0100

    ajout des déplacements de base du joueur

---
+++ Projet-NSI/Assets/Scripts/DeplacementJoueur.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementJoueur : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float[] speed =  {0.05f, 0.05f, 5f};
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input vertical et horizontal correspond aux flèches du clavier
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Le joueur avance en fonction des inputs horizontaux et verticaux
+        transform.Translate(Vector3.right * horizontalInput * speed[0]);
+        transform.Translate(Vector3.forward * verticalInput * speed[1]);
+
+        // La touche espace (space) pour sauter 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * speed[2]);
+        }
+    }
+}
