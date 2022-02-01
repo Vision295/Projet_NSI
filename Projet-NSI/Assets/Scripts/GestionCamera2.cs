fce84430bd98be638a884ced7cb5428436cb8f3c
EtienneEscofier <etienne.escofier@wanadoo.fr>
2022-02-01 10:46:26 +0100

    Caméra
    Scripts gestion caméra

---
+++ Projet-NSI/Assets/Scripts/GestionCamera2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionCamera2 : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x < 10)
+        {
+            transform.position = new Vector3(0, 5, -5);
+        }
+        else if (player.transform.position.x > 10)
+        {
+            transform.position = new Vector3(15, 5, -5);
+        }
+    }
+}
