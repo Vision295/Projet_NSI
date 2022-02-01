93f70d57009f5d5285ca208403b4d4090cfb5877
Vision295 <78678991+Vision295@users.noreply.github.com>
2022-02-01 12:01:43 +0100

    ajout du script déplacement caméra 2

---
+++ Projet-NSI/Assets/Scripts/CollisionJoueur.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionJoueur : MonoBehaviour
+{
+    private GameObject cam;
+    private GestionCamera2 scriptCam;
+    private Vector3 offset = new Vector3(0, 2, -5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Camera");
+        scriptCam = cam.GetComponent<GestionCamera2>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "cameraCheckpoint")
+        {
+            scriptCam.position = other.transform.position + offset;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
