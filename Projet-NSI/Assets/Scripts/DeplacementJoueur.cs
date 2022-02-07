d6841c8366b8d34848f8662dcede6286ffd8afc9
Vision295 <78678991+Vision295@users.noreply.github.com>
2022-02-07 16:44:56 +0100

    déplacement fluide
    attention à comprendre le cc.isgrounded

--- Projet-NSI/Assets/Scripts/DeplacementJoueur.cs
+++ Projet-NSI/Assets/Scripts/DeplacementJoueur.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class DeplacementJoueur : MonoBehaviour
 {
-    private float horizontalInput;
-    private float verticalInput;
-    private float[] speed =  {5f, 5f, 7f};
-    private Rigidbody rb;
-    private bool isGrounded;
-    private bool inputJump;
+    private float horizontalInput, verticalInput;
+    private float[] speed =  {5f, 5f, 3.5f};
+    private CharacterController cc;
+    private bool isGrounded, inputJump;
+    private Vector3 mouvement;
+    private float gravity = 9.18f, directionY; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -26,22 +27,21 @@ public class DeplacementJoueur : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // Le joueur avance en fonction des inputs horizontaux et verticaux
-        rb.AddForce(Vector3.forward * verticalInput * speed[0]);
-        rb.AddForce(Vector3.right * horizontalInput * speed[1]);
+        mouvement = new Vector3(horizontalInput, 0, verticalInput);
         // transform.Rotate(0, horizontalInput * Time.deltaTime, 0);
 
         // La touche espace (space) pour sauter 
-        if (!inputJump && isGrounded)
+        // teser le cc.isGrounded !!!!!!
+        inputJump = Input.GetKeyDown(KeyCode.Space);
+        
+        if (inputJump)
         {
-            inputJump = Input.GetKeyDown(KeyCode.Space);
+            directionY = speed[2];
         }
-
-        if (inputJump && isGrounded)
-        {
-            rb.AddForce(Vector3.up * speed[2]);
-            inputJump = false;
-            isGrounded = false;
-        }
+        directionY -= gravity * Time.deltaTime;
+        mouvement.y = directionY;
+        Debug.Log(mouvement.y);
+        cc.Move(mouvement * Time.deltaTime * 5f);
     }
     void OnCollisionExit(Collision other)
     {
