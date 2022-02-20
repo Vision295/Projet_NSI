3248541c89cdbafe0b052901a95b00530aba54db
Vision295 <78678991+Vision295@users.noreply.github.com>
2022-02-20 12:22:22 +0100

    ajout de pieds au joueur (fonctionnel)

--- Projet-NSI/Assets/Scripts/DeplacementJoueur.cs
+++ Projet-NSI/Assets/Scripts/DeplacementJoueur.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +10,7 @@ public class DeplacementJoueur : MonoBehaviour
     private bool inputJump;
     private Vector3 mouvement;
     private Vector3 rotation;
-    private bool isGrounded = true;
+    public IsGrounded pied;
     
     // Start is called before the first frame update
     void Start()
@@ -29,31 +29,21 @@ public class DeplacementJoueur : MonoBehaviour
 
         // Le joueur avance en fonction des inputs horizontaux et verticaux
         mouvement = new Vector3(horizontalInput, 0, verticalInput);
-        rb.AddRelativeForce((mouvement * speed[0] - rb.velocity) * rb.mass, ForceMode.Force);
+        if(pied.isGrounded)
+        {
+            rb.AddRelativeForce((mouvement * speed[0] - rb.velocity) * rb.mass, ForceMode.Force);
+        }
 
         // La touche espace (space) pour sauter 
         inputJump = Input.GetKeyDown(KeyCode.Space);
 
-        if (inputJump && isGrounded)
+        if (inputJump && pied.isGrounded)
         {
             rb.AddForce(Vector3.up * speed[1] * rb.mass, ForceMode.Impulse);
         }
-        if (!isGrounded)
+        if (!pied.isGrounded)
         {
             rb.AddForce(Vector3.down * speed[1] * 0.5f * rb.mass, ForceMode.Force);
         }
-    }
-    void OnCollisionStay(Collision other)
-    {
-        isGrounded = true;
-    }
-
-    void OnCollisionEnter(Collision other)
-    {  
-        isGrounded = true;
-    }
-    void OnCollisionExit(Collision other)
-    {
-        isGrounded = false;
     }
 }
---
+++ Projet-NSI/Assets/Scripts/IsGrounded.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGrounded : MonoBehaviour
+{
+    public bool isGrounded;
+    public GameObject joueur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = joueur.transform.position + new Vector3(0, -0.82f, 0);
+    }
+    void OnCollisionStay(Collision other)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {  
+        isGrounded = true;
+    }
+    void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
+    }
+
+}
