e240f21ce11c6b0cf6822e6f51b02591c94a5818
Vision295 <78678991+Vision295@users.noreply.github.com>
2022-04-07 13:53:12 +0200

    ajout de commentaire + reglement d'un petit bug

--- Projet-NSI/Assets/Scripts/GénéProcé/PopManager.cs
+++ Projet-NSI/Assets/Scripts/GénéProcé/PopManager.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class PopManager : MonoBehaviour
 {
+    // liste des plateformes
     public GameObject[] listePF;
+    
+    // position de la première plateforme à apparaitre
     public float positionX = 95;
     public float positionY = 60;
     public float positionZ = 6;
+
+    // décalage entre les plateformes générées
     public float ecartZ = 10;
+    public int PFIndex;
+    
+    // à changer
     public float startDelay = 5;
     public float spawnInterval = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        // InvokeRepeating("PopRandomPF", startDelay, spawnInterval);
+        InvokeRepeating("PopRandomPF", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void PopRandomPF(GameObject précédente)
     {
-        // positionX = positionX - 25;
-        // positionY = positionY + 8*Random.Range(0, 1)-4;
-        // positionZ = positionZ + Random.Range(-ecartZ, ecartZ);
-        // Vector3 spawnPosition = new Vector3(positionX, positionY, positionZ);
+        positionX = positionX - 25;
+        positionY = positionY + 8*Random.Range(0, 1)-4;
+        positionZ = positionZ + Random.Range(-ecartZ, ecartZ);
+        Vector3 spawnPosition = new Vector3(positionX, positionY, positionZ);
 
-        // PFIndex = Random.Range(0, listePF.Length);
+        PFIndex = Random.Range(0, listePF.Length);
+        Debug.Log(positionX);
+        Debug.Log(positionY);
+        Debug.Log(positionZ);
         // Instantiate(listePF[PFIndex], spawnPosition, listePF[PFIndex].transform.rotation);
     }
 }
