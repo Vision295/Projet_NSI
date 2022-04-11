8ba5661c96c2a4de53016d59b2299cc62fb8569b
Vision295 <78678991+Vision295@users.noreply.github.com>
2022-04-11 15:19:22 +0200

    mtn le script destroy fonctionne

--- Projet-NSI/Assets/Scripts/GénéProcé/Destroy.cs
+++ Projet-NSI/Assets/Scripts/GénéProcé/Destroy.cs
@@ -5,20 +5,9 @@ using UnityEngine;
 public class Destroy : MonoBehaviour
 {
     public PopManager popmanag;
-    public GameObject plateforme;
-    void Detruit()
-    {
-        Destroy(plateforme);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        plateforme = gameObject;
-        if(popmanag.destroy)
-        {
-            popmanag.destroy = false;
-            Invoke("Detruit", 5f);
-        }
+        Destroy(gameObject, 5);
     }
 }
